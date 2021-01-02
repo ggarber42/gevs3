bb80afef2180d5031362f8fc009b05d652749b0c
ggarber <ggarber@protonmail.com>
2021-01-02 11:35:00 -0300

    arrumando o cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,15 +2,9 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+import "module-that-imports-styles.js"
+import "styles.scss"
+import "../other-styles.css"
 
-CMS.registerMediaLibrary(uploadcare)
+
 CMS.registerMediaLibrary(cloudinary)
-
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
