34b2d77d127a2a3e6312b357054f1de090d039e9
ggarber <ggarber@protonmail.com>
2021-01-02 11:46:48 -0300

    removendo import  do cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,8 +2,6 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
-import "module-that-imports-styles.js"
-import "styles.scss"
 
 
 
